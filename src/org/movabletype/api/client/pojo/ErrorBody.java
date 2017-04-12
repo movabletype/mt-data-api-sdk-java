8000c2ee86216765bfef31dd9710fbc93efa77fa
kanai <kanai@kanaiMac.local>
2017-04-12 15:04:29 +0900

    Duplicate import removed.

--- src/org/movabletype/api/client/pojo/ErrorBody.java
+++ src/org/movabletype/api/client/pojo/ErrorBody.java
@@ -2,15 +2,7 @@ package org.movabletype.api.client.pojo;
 
 import java.util.HashMap;
 import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import java.util.HashMap;
-import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
